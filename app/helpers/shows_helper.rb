4b1b503e22b8d98d3110f93f6ca00e26d8d8d8f6
Kris E. Lentz <kris.e.lentz@gmail.com>
2019-04-18 17:24:49 -0500

    moved past and future show functionality to helper

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,2 +1,17 @@
 module ShowsHelper
+  
+  def date_helper shows
+    @upcoming_shows = Array.new
+    @past_shows = Array.new
+    
+    shows.each do |show|
+      if show[:date] > Date.today
+        @upcoming_shows.push(show)
+      else
+        @past_shows.push(show) 
+      end
+    end  
+  end
+
+
 end
