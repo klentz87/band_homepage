249224f6c62894476f95f408209dc0ee29cb6916
Kris E. Lentz <kris.e.lentz@gmail.com>
2019-04-30 21:50:15 -0500

    refactor category_path_helper method

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,11 +1,5 @@
 module ProductsHelper
   def category_path_helper product
-    if Category.find(product.category_id).name == 'ALBUMS'
-      albums_path
-    elsif Category.find(product.category_id).name == 'EPS'
-      eps_path
-    elsif Category.find(product.category_id).name == 'OTHER'
-      other_path
-    end
+    Category.find(product.category_id).name.downcase
   end
 end
