750abad3120bb3a292a60873921a68729dd2c50c
Kris E. Lentz <kris.e.lentz@gmail.com>
2019-05-02 23:41:03 -0500

    create grid layout for products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,6 @@ class Product < ApplicationRecord
 
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '350')
   end
 end
