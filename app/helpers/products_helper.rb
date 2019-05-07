60b589356b4fde91260d70bd228e7fad0345e14f
Kris E. Lentz <kris.e.lentz@gmail.com>
2019-05-07 17:01:04 -0500

    delete repeat code in products_helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -22,19 +22,4 @@ module ProductsHelper
       }
     ]
   end
-
-  def nav_helper style, tag_type, item_array
-    nav_links = ''
-
-    item_array.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
-    end
-
-    nav_links.html_safe
-  end
-
-  def active? path
-    "active" if current_page? path
-  end
-
 end
